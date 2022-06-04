918e07a88e79e8fe6087f4ea948039f5d8d3f422
Jonny Gamba <gambonny@gmail.com>
2022-06-04 10:44:24 +0200

    better buffers movement

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,6 @@
 -- Telescope
 vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files <CR>", {})
-vim.api.nvim_set_keymap("n", "<Leader>tb", ":Telescope buffers <CR>", {})
+vim.api.nvim_set_keymap("n", "<Leader>tb", ":Telescope buffers theme=dropdown <CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>tm", ":Telescope marks <CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>tg", ":Telescope grep_string<CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>tl", ":Telescope live_grep<CR>", {})
@@ -9,3 +9,8 @@ vim.api.nvim_set_keymap("n", "<Leader>td", ":Telescope git_bcommits<CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>th", ":Telescope git_branches<CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>ts", ":Telescope git_status<CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>tz", ":Telescope current_buffer_fuzzy_find<CR>", {})
+
+
+-- Buffers
+vim.api.nvim_set_keymap("n", "[b", ":BufferLineCycleNext <CR>", {})
+vim.api.nvim_set_keymap("n", "]b", ":BufferLineCyclePrev <CR>", {})
